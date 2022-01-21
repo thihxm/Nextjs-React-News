e6b4a1b966e98003ed40888737dde500716bc9e2
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-21 16:11:47 -0300

    Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import SubscribeButton from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
 
 const Home: NextPage = () => {
   return (
@@ -7,9 +10,22 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <main className={styles['content-container']}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
