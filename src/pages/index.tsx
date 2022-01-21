489c5e7d388b6e718f28584765b5207a67c4aabd
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-21 11:43:08 -0300

    Base app

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
+  )
+}
+
+export default Home
