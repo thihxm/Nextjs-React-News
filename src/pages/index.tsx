95078dc0ad7e8ec8526e6a9de700311a3a015f5c
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-21 18:21:13 -0300

    Index using SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import SubscribeButton from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -40,7 +40,7 @@ const Home: NextPage<HomeProps> = ({ product }) => {
 
 export default Home
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KKSdyL70ay78IfpJO1ZJYb6')
 
   const product = {
@@ -53,5 +53,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { product },
+    revalidate: 60 * 60 * 24, // 24h
   }
 }
