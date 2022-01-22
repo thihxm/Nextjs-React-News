584ddd56f9e6a81b69eb485c10732f75f9815a7c
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-22 11:54:27 -0300

    Save user to FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,9 +10,36 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       authorization: {
         params: {
-          scope: 'read:user',
+          scope: 'read:user user:email',
         },
       },
     }),
   ],
+  secret: `${process.env.NEXTAUTH_SECRET}`,
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(`${email}`))
+              )
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(`${email}`)))
+          )
+        )
+
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+    },
+  },
 })
